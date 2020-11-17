b80bd439ebfc13d7e9c639b3c048f00a11bf25d7
Wesley YS <wesleyyds@hotmail.com>
2020-11-17 14:08:51 -0300

    feat: FeatureFlag support enabled on all routes

--- Controllers/RepositoryController.cs
+++ Controllers/RepositoryController.cs
@@ -7,12 +7,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using WebAPIClient.Models;
+using Microsoft.FeatureManagement;
 
 namespace WebAPIClient.Controllers {
     [ApiController]
     [Route("repositories")]
     public class RepositoryController : ControllerBase {
-
+        private IFeatureManager featureManager;
         private readonly IMemoryCache _cache;
         public RepositoryController(IMemoryCache cache)
         {
@@ -25,30 +26,35 @@ namespace WebAPIClient.Controllers {
         [Route("{company}")]
         public async Task<List<Repository>> ProcessRepositories(string company)
         {
-            var cacheEntry = await _cache.GetOrCreateAsync(company, async entry =>
+            if (await featureManager.IsEnabledAsync("CacheFeatureFlag"))
             {
-                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10);
-                entry.SetPriority(CacheItemPriority.High);
-                
-                return await ListRepositoriesByName(company);
-            });
-            
-            return cacheEntry;
+                var cacheEntry = await _cache.GetOrCreateAsync(company, async entry =>
+                {
+                    entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10);
+                    entry.SetPriority(CacheItemPriority.High);
+                    
+                    return await ListRepositoriesByName(company);
+                });
+                return cacheEntry;
+            }
+            else return await ListRepositoriesByName(company);            
         }
 
         [HttpGet]
         [Route("")]
         public async Task<List<Repository>> ProcessRepositoriesByBody([FromBody] Organization org)
         {
-            var cacheEntry = await _cache.GetOrCreateAsync(org.company, async entry =>
+            if (await featureManager.IsEnabledAsync("CacheFeatureFlag"))
             {
-                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10);
-                entry.SetPriority(CacheItemPriority.High);
-                
-                return await ListRepositoriesByName(org.company);
-            });
-            
-            return cacheEntry;
+                var cacheEntry = await _cache.GetOrCreateAsync(org.company, async entry =>
+                {
+                    entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10);
+                    entry.SetPriority(CacheItemPriority.High);
+                    
+                    return await ListRepositoriesByName(org.company);
+                });            
+                return cacheEntry;
+            } else return await ListRepositoriesByName(org.company);
         }
 
         private async Task<List<Repository>> ListRepositoriesByName(string companyName)
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
 using Microsoft.OpenApi.Models;
 
 namespace WebAPIClient
@@ -26,7 +27,7 @@ namespace WebAPIClient
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddFeatureManagement();
             services.AddControllers();
             services.AddMemoryCache();
             services.AddSwaggerGen(c =>
